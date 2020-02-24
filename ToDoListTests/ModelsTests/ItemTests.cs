ac99c93b4f3a6b7e8589ca21fb33609ed9bca4ed
silvermistshadow <bmead72@gmail.com>
2020-02-24 11:13:47 -0800

    add tests and code

---
+++ ToDoListTests/ModelsTests/ItemTests.cs
@@ -0,0 +1,65 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ToDoListTests
+{
+  [TestClass]
+  public class ItemTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Item.ClearAll();
+    }
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+        Item newItem = new Item("test");
+        Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+        // Arrange
+        string description = "Walk the dog.";
+        Item newItem = new Item(description);
+
+        // Act
+        string result = newItem.Description;
+
+        // Assert
+        Assert.AreEqual(description, result);
+    }
+
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+        // Arrange
+        string description = "Walk the dog.";
+        Item newItem = new Item(description);
+
+        // Act
+        string updatedDescription = "Do the dishes";
+        newItem.Description = updatedDescription;
+        string result = newItem.Description;
+
+        // Assert
+        Assert.AreEqual(updatedDescription, result);
+    }
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+        // Arrange
+        List<Item> newList = new List<Item>();
+
+        // Act
+        List<Item> result = Item.GetAll();
+
+        // Assert
+        CollectionAssert.AreEqual(newList, result);
+    }
+
+  }
+}
